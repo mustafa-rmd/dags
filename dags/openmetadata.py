27ed5ff12bd831474e9fd129208e718182fee67d
mustafa-alramadan <mustafa.alramadhan@fathom.io>
2022-10-24 13:47:54 +0300

    remove openmetadata test

--- dags/openmetadata.py
+++
@@ -1,6 +0,0 @@
-from airflow import DAG
-from openmetadata_managed_apis.workflows import workflow_factory
-
-workflow = workflow_factory.WorkflowFactory.create("/opt/bitnami/airflow/dags/postgres_metadata_gxkcihO0.json")
-workflow.generate_dag(globals())
-dag = workflow.get_dag()
